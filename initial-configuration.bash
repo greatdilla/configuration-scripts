7f6e60e2e41b652c05576e27d3cf911acfcbd458
greatdilla <63782505+greatdilla@users.noreply.github.com>
2020-04-17 12:03:02 +0800

    Update initial-configuration.bash
    Missed the line for $EUID.

--- initial-configuration.bash
+++ initial-configuration.bash
@@ -97,6 +97,7 @@ function checkV16() {
 
 function checkRoot() {
     ## run-as root?
+    EUID=$(id -u)
     if [[ $EUID -ne 0 ]]; then
         echo -e "${RED}$0 must be run as root.${NC}"
         exit 1
